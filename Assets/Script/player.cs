198d65b27d854de99ce36a50928724b1d5fae4ba
KrishnaXavier <tsixav@gmail.com>
2017-06-11 21:45:00 -0300

    cenario base e movimentação prontos

---
+++ Assets/Script/player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+
+	private Rigidbody2D playerRb;
+	private SpriteRenderer playerSprite;
+	public float velocidade;
+	public bool flipX = false;
+
+
+	// Use this for initialization
+	void Start () {
+		playerRb = GetComponent<Rigidbody2D> ();
+		playerSprite = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown (0)) {			
+			velocidade *= -1;
+			flipX = !flipX;
+			playerSprite.flipX = flipX;
+		}
+
+		playerRb.velocity = new Vector2 (velocidade, 0);
+
+		
+	}
+}
